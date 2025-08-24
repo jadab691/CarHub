2d660a91a0c149ab8fb9db92ea34c780f9fe8fc4
jadab691 <sarkarjadab83@gmail.com>
2025-08-25 01:48:51 +0600

    Cars update

--- frontend/src/cars/Cars.jsx
+++ frontend/src/cars/Cars.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Car from "../components/Car";
-import list from "../../public/list.json"
 
 function Cars() {
   return (
-    console.log(list),
     <>
       <Navbar />
-      <div className="min-h-screen flex flex-col items-center justify-center">
+      <div>
         <Car />
       </div>
       <Footer />
