0b0f95f2004eb2f1fb257e3f10104cc2ae5c8961
jadab691 <sarkarjadab83@gmail.com>
2025-08-24 13:31:15 +0600

    Update display text

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -49,51 +49,8 @@ function Display() {
 
         <div className="pt-1 pb-10">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
-            nesciunt quasi ducimus eligendi eveniet vero. Accusantium odio
-            incidunt minus earum minima tempora non ex nisi. Ad doloribus magni
-            fugiat id facilis debitis, officiis recusandae explicabo!
-            Voluptatibus fugit nisi ducimus? Consectetur laudantium itaque
-            magnam nesciunt sint explicabo magni suscipit porro quam, excepturi
-            qui repellat maiores earum eius veritatis similique totam! Ut,
-            explicabo nostrum repellendus eveniet nihil laudantium sunt
-            repudiandae consequatur deleniti quam esse delectus fugit veritatis
-            non magnam obcaecati ex illum, cumque necessitatibus rerum! Earum
-            officia voluptate non, sed natus ullam illum molestias quo beatae
-            harum quasi laboriosam eius vel minus, iure explicabo ad veritatis
-            illo dolore. Mollitia exercitationem nesciunt excepturi iste eos
-            voluptas minima, quibusdam nobis. Numquam rerum, ullam corrupti,
-            ratione suscipit laudantium, asperiores libero voluptates vero ipsum
-            similique incidunt tenetur maxime blanditiis et. Dolorem, iste
-            soluta quaerat, sit possimus, explicabo laboriosam autem enim eius
-            ea dolore molestias distinctio quisquam voluptates quos sequi.
-            Libero dignissimos consectetur est officia inventore, expedita sunt
-            quisquam laudantium? Dicta aliquid in impedit ex voluptatibus
-            quisquam soluta exercitationem ipsa, iste reprehenderit nihil
-            sapiente doloremque totam blanditiis debitis est asperiores hic
-            quaerat commodi cumque! Esse, facilis odit? Numquam quo dolore
-            perspiciatis, aliquid ab aspernatur minima facilis qui sapiente
-            magni. Harum fuga fugit eligendi nostrum? Odit natus voluptatum ex
-            numquam voluptates eum beatae modi ab nesciunt excepturi architecto
-            iusto, aut enim asperiores placeat ipsum omnis labore perferendis
-            officia ullam. Voluptatibus eius repudiandae odit dolorem culpa non,
-            iure dolor magni tenetur quia, ex sed. In, maxime, perspiciatis ipsa
-            enim a tempore ipsum, praesentium reiciendis laudantium aliquam
-            adipisci accusantium delectus incidunt nihil ex expedita nam tenetur
-            quis aperiam dolores. Earum voluptates neque, eveniet adipisci, vero
-            quasi sunt dolore nemo est aspernatur, porro ipsam unde
-            necessitatibus omnis! Quos laudantium earum maiores mollitia
-            incidunt at dicta magnam et dolores delectus, rem nam dignissimos
-            facilis a officiis ipsum quas tempore necessitatibus repudiandae
-            sint iure soluta. Quae magni eveniet molestias voluptatum sit nobis
-            praesentium eum quod numquam dicta maiores qui, et cum totam, vitae
-            asperiores? Totam commodi nobis natus inventore sequi adipisci
-            similique laborum repellendus et tenetur aspernatur quis eos beatae
-            excepturi voluptas, modi iusto magnam, doloremque ipsam? Quidem
-            perferendis nostrum quis sunt ducimus explicabo fugiat tenetur
-            quibusdam error dolor obcaecati possimus harum ipsum eveniet commodi
-            voluptatem, rem eum. Quam libero facere accusamus aliquam assumenda
-            hic magni.
+            Lorem, ipsum dolor. Harum, nam alias. Magni, enim ipsa! Adipisci,
+            veniam beatae?
           </p>
         </div>
         {/* card container  */}
